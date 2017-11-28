75aa3e35456dcdba1334fd1bc510d45d3f57f331
Rebecca Bratu <beckybratu@gmail.com>
2017-11-28 04:00:34 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,33 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+   @figures = Figure.all
+   erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    # @figure = Figure.create(:name => params["figure"]["name"])
+    # @landmark = Landmark.find_or_create_by(name: params["landmark"])
+    # @figure.landmark = @landmark
+    #
+    # @title = Title.find_or_create_by(name: params["title"])
+    # @figure.title = @title
+    # redirect to "/figures/#{@figure.name}"
+    figure = Figure.create(params[:figure])
+    figure.titles.build(params[:title]) unless params[:title][:name].empty?
+    figure.landmarks.build(params[:landmark]) unless params[:landmark][:name].empty?
+    figure.save
+    redirect("/figures/#{figure.id}")
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id:params[:id])
+    erb :"/figures/show"
+  end
+
+
 end
